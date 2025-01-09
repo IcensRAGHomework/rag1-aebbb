fb5920f65a7421ea95e523ade18853ad8503bf55
aebbb <aebbb.ai@gmail.com>
2025-01-09 16:02:43 +0800

    Update generate_hw01

--- student_assignment.py
+++ student_assignment.py
@@ -1,27 +1,45 @@
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+
 import json
-import traceback
+import re
+from langchain_core.output_parsers import JsonOutputParser
+from pydantic import BaseModel, Field
+from typing import List
 
-from model_configurations import get_model_configuration
+class Holiday_Item(BaseModel):
+    """Information about a holiday."""
+    date: str = Field(..., description="Date of holiday")
+    name: str = Field(..., description="Name of holiday.")
 
-from langchain_openai import AzureChatOpenAI
-from langchain_core.messages import HumanMessage
+class Final_Result(BaseModel):
+    """Identifying information about all holidays in specific month."""
+    Result: List[Holiday_Item]
 
-gpt_chat_version = 'gpt-4o'
-gpt_config = get_model_configuration(gpt_chat_version)
+# Custom parser
+def extract_json(message: AIMessage) -> List[dict]:
+    """Extracts JSON content from a string where JSON is embedded between \`\`\`json and \`\`\` tags.
+
+    Parameters:
+        text (str): The text containing the JSON content.
+
+    Returns:
+        list: A list of extracted JSON strings.
+    """
+    text = message.content
+    # Define the regular expression pattern to match JSON blocks
+    pattern = r"\`\`\`json(.*?)\`\`\`"
+
+    # Find all non-overlapping matches of the pattern in the string
+    matches = re.findall(pattern, text, re.DOTALL)
+
+    # Return the list of matched JSON strings, stripping any leading or trailing whitespace
+    try:
+        return [json.loads(match.strip()) for match in matches]
+    except Exception:
+        raise ValueError(f"Failed to parse: {message}")
 
 def generate_hw01(question):
-    pass
-    
-def generate_hw02(question):
-    pass
-    
-def generate_hw03(question2, question3):
-    pass
-    
-def generate_hw04(question):
-    pass
-    
-def demo(question):
     llm = AzureChatOpenAI(
             model=gpt_config['model_name'],
             deployment_name=gpt_config['deployment_name'],
@@ -30,11 +48,26 @@ def demo(question):
             azure_endpoint=gpt_config['api_base'],
             temperature=gpt_config['temperature']
     )
-    message = HumanMessage(
-            content=[
-                {"type": "text", "text": question},
-            ]
-    )
-    response = llm.invoke([message])
-    
-    return response
+
+    # Prompt
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "當使用者針對特定年份的台灣行事曆做查詢, 請列出所有該月份的國定紀念日"
+                "Output your answer as JSON that  "
+                "matches the given schema: \`\`\`json\n{schema}\n\`\`\`. "
+                "Make sure to wrap the answer in \`\`\`json and \`\`\` tags",
+            ),
+            ("human", "{query}"),
+        ]
+    ).partial(schema=Final_Result.model_json_schema())
+
+    # Set up a parser + inject instructions into the prompt template.
+    parser = JsonOutputParser(pydantic_object=Final_Result)
+
+    chain = prompt | llm | parser
+    response = chain.invoke({"query": question})
+
+    # convert to json format
+    return json.dumps(response, ensure_ascii = False)
