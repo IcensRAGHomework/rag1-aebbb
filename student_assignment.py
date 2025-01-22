8e1ce8ef2631d5fabc54b7af47758c1839f086e3
aebbb <aebbb.ai@gmail.com>
2025-01-22 16:36:56 +0800

    Update student_assignment.py

--- student_assignment.py
+++ student_assignment.py
@@ -1,13 +1,24 @@
-import traceback
-
 from model_configurations import get_model_configuration
 
 from langchain_openai import AzureChatOpenAI
-from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+
+from langchain_core.messages import HumanMessage, BaseMessage, SystemMessage, AIMessage
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.documents import Document
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.runnables import (
+    RunnableLambda,
+    ConfigurableFieldSpec,
+    RunnablePassthrough,
+)
+
 from pydantic import BaseModel, Field
 from typing import List
+
+import traceback
 import requests
 import json
 import re
@@ -120,3 +131,72 @@ def generate_hw02(question):
     response = chain.invoke({"inputdata": cal_json})
 
     return response.content[7:-3]
+
+
+
+class InMemoryHistory(BaseChatMessageHistory, BaseModel):
+    messages: List[BaseMessage] = Field(default_factory=list)
+
+    def add_messages(self, messages: List[BaseMessage]) -> None:
+        self.messages.extend(messages)
+
+    def clear(self) -> None:
+        self.messages = []
+
+store = {}
+
+def get_by_session_id(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = InMemoryHistory()
+    return store[session_id]
+
+def generate_hw03(question2, question3):
+    cal_list = generate_hw02(question2)
+
+    llm = AzureChatOpenAI(
+        model=gpt_config['model_name'],
+        deployment_name=gpt_config['deployment_name'],
+        openai_api_key=gpt_config['api_key'],
+        openai_api_version=gpt_config['api_version'],
+        azure_endpoint=gpt_config['api_base'],
+        temperature=gpt_config['temperature']
+    )
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("ai", "{holiday_list}"),
+        MessagesPlaceholder(variable_name="history"),
+        ("human", "{question}"),
+    ])
+    chain = prompt | llm
+
+    chain_with_history = RunnableWithMessageHistory(
+        chain,
+        get_by_session_id,
+        input_messages_key="question",
+        history_messages_key="history",
+    )
+
+    result1 = chain_with_history.invoke(
+        {"holiday_list": cal_list,
+         "question": question3},
+        config={"configurable": {"session_id": "cal_chat_history"}}
+    )
+
+    result_add_or_not = chain_with_history.invoke(
+        {"holiday_list": cal_list,
+         "question": "這節日如果不在之前的清單, 並且需要被加入, 請回答 true, 反之則回答 false"},
+        config={"configurable": {"session_id": "cal_chat_history"}}
+    )
+
+    result_reason = chain_with_history.invoke(
+        {"holiday_list": cal_list,
+         "question": "只用一行來解釋一下需要加入或不加入的原因, 並列出目前已存在的所有節日名稱"},
+        config={"configurable": {"session_id": "cal_chat_history"}}
+    )
+    reason = result_reason.content.replace("\"", "") # remove " inside string
+
+    final_info = " \"add\": {0},   \"reason\": \"{1}\" "
+    final_response = final_info.format(result_add_or_not.content.lower(), reason)
+    final_response = " { \"Result\": {  " + final_response + " }  }"
+
+    return final_response
