aac03a94faac886551235d96e059ccdadf11bd90
aebbb <aebbb.ai@gmail.com>
2025-01-22 16:32:35 +0800

    Update student_assignment.py

--- student_assignment.py
+++ student_assignment.py
@@ -1,23 +1,19 @@
-
-import json
 import traceback
 
 from model_configurations import get_model_configuration
 
 from langchain_openai import AzureChatOpenAI
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel, Field
+from typing import List
+import requests
+import json
+import re
 
 gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
-
-from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
-from langchain_core.prompts import ChatPromptTemplate
-
-import json
-import re
-from langchain_core.output_parsers import JsonOutputParser
-from pydantic import BaseModel, Field
-from typing import List
 
 class holiday(BaseModel):
     date: str = Field(..., description="Date of holiday")
@@ -81,3 +77,46 @@ def generate_hw01(question):
 
     # convert to json format
     return json.dumps(response, ensure_ascii = False)
+
+
+def generate_hw02(question):
+
+    llm = AzureChatOpenAI(
+        model=gpt_config['model_name'],
+        deployment_name=gpt_config['deployment_name'],
+        openai_api_key=gpt_config['api_key'],
+        openai_api_version=gpt_config['api_version'],
+        azure_endpoint=gpt_config['api_base'],
+        temperature=gpt_config['temperature']
+    )
+    query_year = llm.invoke([
+        SystemMessage(content="只回答問題中的年份並用數字表示"),
+        HumanMessage(content=question),
+    ]).content
+    query_month = llm.invoke([
+        SystemMessage(content="只回答問題中的月份並用數字表示"),
+        HumanMessage(content=question),
+    ]).content
+
+    api_url = "https://calendarific.com/api/v2/holidays?&api_key=JQeWnmY3xqc6y2jRtEhdL58tQY3lKdA5&country=TW&language=zh&year="+str(query_year)+"&month="+str(query_month)
+    webapi_response = requests.get(api_url)
+    cal_json = webapi_response.json()
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "Output your answer as JSON that  "
+                "matches the given schema: \`\`\`json\n{schema}\n\`\`\`. "
+                "Make sure to wrap the answer in \`\`\`json and \`\`\` tags",
+            ),
+            ("human", "{inputdata}"),
+        ]
+    ).partial(schema=holiday_list.model_json_schema())
+
+    parser = JsonOutputParser(pydantic_object=holiday_list)
+
+    chain = prompt | llm
+    response = chain.invoke({"inputdata": cal_json})
+
+    return response.content[7:-3]
